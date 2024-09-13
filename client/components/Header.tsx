809a20deda81a090d0e9c12ff3d6affdcb9fbb69
RyogoIimura <116061207+RyogoIimura@users.noreply.github.com>
2024-09-13 10:27:45 +0900

    Header スタイル着手

--- client/components/Header.tsx
+++ client/components/Header.tsx
@@ -3,6 +3,7 @@ import { manrope } from "../utils/Fonts";
 import { css } from "@emotion/react";
 
 import { PROJECT } from '../data/AppData'
+import Link from "next/link";
 
 const Header = () => {
 
@@ -10,39 +11,75 @@ const Header = () => {
     <>
       {/* header */}
       <div
-        css={styles.box}
+        css={styles.headerContainer}
       >
-        <div>
-          <p>Header</p>
-          <div></div>
+        <div
+          css={styles.headerContContainer}
+        >
+          <Link href="">▶︎ ご予約はこちらから</Link>
+          <h2
+            className={` ${manrope.className}`}
+            css={styles.headerTitle}
+          >AZUMA GORGE SAUNA</h2>
+          <div>
+            <div
+              css={styles.hamburger}
+            ></div>
+          </div>
         </div>
       </div>
 
+      {/* bg */}
+      <div
+        css={styles.bg}
+      ></div>
       {/* nav menu */}
       <div></div>
-
-      {/* hamburger */}
-      <button>
-        <div>
-          <div></div>
-          <div></div>
-          <div></div>
-        </div>
-      </button>
     </>
   );
 };
 export default Header;
 
 const styles = {
-  box: css`
-    position: relative;
-    width: 300px;
-    height: 300px;
-    background-color: rgba(255, 0, 0, 0.5);
+  headerContainer: css`
+    width: 100%;
+    height: 100px;
+    background-color: ${PROJECT.KEYCOLOR};
+    position: fixed;
+    top: 0;
+    left: 0;
 
-    @media (max-width: ${PROJECT.BP}px) {
-      background-color: rgb(0, 0, 255, 0.5);
+    @media (min-width: ${PROJECT.BP}px) {
+      height: 100px;
+    }
+  `,
+  headerContContainer: css`
+    width: 100%;
+    height: 100%;
+    position: relative;
+
+    @media (min-width: ${PROJECT.BP}px) {
+    }
+  `,
+  headerTitle: css `
+    font-size: 10px;
+    font-weight: 800;
+    color: #fff;
+
+    @media (min-width: ${PROJECT.BP}px) {
+    }
+  `,
+  bg: css `
+    width: 100vw;
+    height: 100vh;
+    background-color: #000;
+    opacity: .5;
+    position: fixed;
+    top: 0;
+    left: 0;
+  `,
+  hamburger: css`
+    @media (min-width: ${PROJECT.BP}px) {
     }
   `
 }
